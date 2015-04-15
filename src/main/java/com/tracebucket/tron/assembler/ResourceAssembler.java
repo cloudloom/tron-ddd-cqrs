ce91d00aabcbc6a5e0294f8cd7c16b8a4ec13046
sadath <syed.sadath@mmpsd.com>
2015-04-15 11:54:10 +0530

    Assembler Working.

--- src/main/java/com/tracebucket/tron/assembler/ResourceAssembler.java
+++ src/main/java/com/tracebucket/tron/assembler/ResourceAssembler.java
@@ -1,5 +1,7 @@
 package com.tracebucket.tron.assembler;
 
+import com.tracebucket.tron.ddd.domain.BaseAggregateRoot;
+import com.tracebucket.tron.ddd.domain.BaseEntity;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,21 +21,24 @@ public class ResourceAssembler<T extends BaseResource, E> {
     @Autowired
     private Mapper mapper;
 
-
     public ResourceAssembler(){
 
     }
 
     public T toResource(E entity, Class<T> resourceClass){
         T resource = mapper.map(entity, resourceClass);
+        if(entity instanceof BaseEntity) {
+            resource.setUid(((BaseEntity) entity).getEntityId().getId());
+        } else if(entity instanceof BaseAggregateRoot) {
+            resource.setUid(((BaseAggregateRoot) entity).getAggregateId().getId());
+        }
         return resource;
     }
 
     public Set<T> toResources(Collection<E> entities, Class<T> resourceClass){
         Set<T> resources = new HashSet<T>();
         for(E entity: entities){
-            T resource = mapper.map(entity, resourceClass);
-            resources.add(resource);
+            resources.add(toResource(entity, resourceClass));
         }
         return resources;
     }
