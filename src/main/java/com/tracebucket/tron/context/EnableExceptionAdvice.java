2558e05e9cc3dcbbf7b8b0619d2bd75002e537c8
sadath <ssmdrepo@gmail.com>
2015-06-15 17:15:05 +0530

    Changed to Version 0.7 Added @EnableExceptionAdvice

--- src/main/java/com/tracebucket/tron/context/EnableExceptionAdvice.java
+++ src/main/java/com/tracebucket/tron/context/EnableExceptionAdvice.java
@@ -14,5 +14,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Import(TronControllerAdvisor.class)
-public @interface EnableControllerAdvisor {
+public @interface EnableExceptionAdvice {
 }
